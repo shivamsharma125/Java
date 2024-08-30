3d75b84d71f0f641d0d802d11f6016dfb3aab89b
Shivam Sharma <shivamsharma8228@gmail.com>
2024-08-30 15:54:36 +0530

    comparing two dates

--- src/main/java/com/shivam/datetime/DateTimeUtil.java
+++ src/main/java/com/shivam/datetime/DateTimeUtil.java
@@ -10,4 +10,19 @@ public class DateTimeUtil {
         double hours = minutes / 60;
         return (int) Math.ceil(hours);
     }
+
+    public static int compareTwoDates(Date date1, Date date2){
+        // the below methods checks time using millis
+
+        return date1.compareTo(date2);
+        // < 0  : Date 1 occurs before Date 2
+        // == 0 : Date 1 is same as Date 2
+        // > 0  : Date 1 occurs after Date 2
+
+//        or
+
+//        date1.before(date2); // date1 occurs before date 2
+//        date1.equals(date2); // date 1 is equal to date 2
+//        date1.after(date2); // date 1 occurs after date 2
+    }
 }
